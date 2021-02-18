f2dbdc40fb0f867abd3edbce61ca72ccd7067f18
John <m@DESKTOP-7TRGB04.localdomain>
2021-02-18 20:56:57 +0800

    mod

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,10 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+echo 'Modify default IP'
 sed -i 's/192.168.1.1/192.168.100.100/g' package/base-files/files/bin/config_generate
 
-# fix vlan ports displaying
+echo 'fix vlan ports displaying'
 sed -i '$a \
 .cbi-section-table-descr { \
     display: table-row !important;\
@@ -23,10 +23,11 @@ echo '修改默认主题'
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
 echo '去除默认bootstrap主题'
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/theme-bootstrap/theme-argon/g' feeds/luci/collections/luci/Makefile
 
-echo '默认关闭WiFi'
-sed -i 's/disabled=0/disabled=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#echo '关闭WiFi'
+#sed -i 's/disabled=0/disabled=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 echo '删除默认zerotier'
 rm -rf ./feeds/packages/net/zerotier

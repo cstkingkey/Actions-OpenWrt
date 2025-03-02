9982c9067c81c9badeed5b7e446c42b898b1423e
cstkingkey <cstkingkey@users.noreply.github.com>
2025-03-02 12:06:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,7 +25,7 @@
 echo 'Add a feed source'
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo 'src-git ppackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 
 # github doesn't support snv anymore.
 #svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
